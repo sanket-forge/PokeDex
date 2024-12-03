7fe862e216521f0cd45a6081cbe98fb847ac8930
sanket-forge <sanketkhatua9861@gmail.com>
2024-12-03 17:03:32 +0530

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Sanket Khatua on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokeDexTests/PokeDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexTests.swift
+//  PokeDexTests
+//
+//  Created by Sanket Khatua on 03/12/24.
+//
+
+import Testing
+@testable import PokeDex
+
+struct PokeDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
